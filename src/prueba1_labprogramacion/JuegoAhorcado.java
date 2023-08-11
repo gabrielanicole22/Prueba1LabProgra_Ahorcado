b42091300ecb2cce9a385c022232d4be4055711f
Gaby <gabrielanicole@unitec.edu>
2023-08-11 13:46:30 -0600

    Se creó Interfaz JuegoAhorcado y clase Abstracta JuegoAhorcadoBase

---
+++ src/prueba1_labprogramacion/JuegoAhorcado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1_labprogramacion;
+
+/**
+ *
+ * @author Gabriela Mejía
+ */
+public interface JuegoAhorcado {
+    public void inicializarPalabraSecreta();
+    public void jugar();
+}
---
+++ src/prueba1_labprogramacion/JuegoAhorcadoBase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1_labprogramacion;
+
+/**
+ *
+ * @author Gabriela Mejia
+ */
+public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    
+    abstract void actualizarPalabraActual(char letra);
+    abstract void verificarLetra(char letra);
+    abstract void hasGanado();
+}
