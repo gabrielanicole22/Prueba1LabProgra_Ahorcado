7892318e2c39344dbf46f959068040f25fa5264d
Gaby <gabrielanicole@unitec.edu>
2023-08-11 14:40:33 -0600

    Se editó función de actualizarPalabraActual

--- src/prueba1_labprogramacion/JuegoAhorcadoAzar.java
+++ src/prueba1_labprogramacion/JuegoAhorcadoAzar.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
  * @author Gabriela Mejía
  */
 class JuegoAhorcadoAzar extends JuegoAhorcadoBase {
+
     private List<String> palabrasPosibles;
     private Random random;
 
@@ -52,15 +53,18 @@ class JuegoAhorcadoAzar extends JuegoAhorcadoBase {
 
     @Override
     void actualizarPalabraActual(char letra) {
-        StringBuilder nuevaPalabraActual = new StringBuilder(palabraActual);
+    }
+
+    public String actualizarPalabraActual(char letra, String palabraSecreta, String palabraActual) {
+        char[] nuevaPalabraActual = palabraActual.toCharArray();
 
         for (int i = 0; i < palabraSecreta.length(); i++) {
             if (palabraSecreta.charAt(i) == letra) {
-                nuevaPalabraActual.setCharAt(i, letra);
+                nuevaPalabraActual[i] = letra;
             }
         }
 
-        palabraActual = nuevaPalabraActual.toString();
+        return new String(nuevaPalabraActual);
     }
 
     @Override
